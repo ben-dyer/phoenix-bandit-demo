8e13294637232b9da8c3c9a6c61b3f0f7f0d6299
Ben Dyer <43922444+ben-dyer@users.noreply.github.com>
2021-11-09 17:37:38 +0900

    first commit

---
+++ lib/fawkes.ex
@@ -0,0 +1,18 @@
+defmodule Fawkes do
+  @moduledoc """
+  Documentation for `Fawkes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fawkes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
